13a84c9e9a13115a0ff06aaf9a0fec64d356a58a
Clement Bogyah <zenerboggyah7@gmail.com>
2022-12-08 01:59:04 -0800

    file added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+/**
+   Main entry point here
+   This print to the screen using puts
+   Return 0 for success
 
+ */
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
-
   return 0;
 }
