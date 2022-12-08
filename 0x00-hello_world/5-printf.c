07a9b4d00b1b83a7de015812ea63e2eacf0bae57
Clement Bogyah <zenerboggyah7@gmail.com>
2022-12-08 02:38:15 -0800

    file added

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *   main - Entry point of the pogram
+ *  Main - Entry point of programming
  *
  *  Description - with proper grammar, but the outcome is a piece of art,
  *
  *  Usage - using printf
- */
+*/
 int main(void)
 {
   printf("with proper grammar, but the outcome is a piece of art,\n");
