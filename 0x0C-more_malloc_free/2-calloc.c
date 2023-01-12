dd450c9f651ee7aef8a43b3e13ca3bdb42ba92af
Clement Bogyah <zenerboggyah7@gmail.com>
2023-01-12 04:42:52 -0800

    file

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,28 +1,27 @@
 #include <stdlib.h>
-#include <stdio.h>
-/**
- * array_range - Create array of integers filled with numbers from min to max
- * @min: First number in the array
- * @max: Largest number in the array
- *
- * Return: Pointer to new array, NULL if failed
- */
-int *array_range(int min, int max)
-{
-	int *ar;
-	int i, size, n;
 
-	if (min > max)
+/**
+ * _calloc - Allocate memory for array of nmemb elements of size bytes
+ * @nmemb: Number of elemnts
+ * @size: Size in bytes of elements
+ *
+ * Return: Pointer to new memory, NULL if it fails
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ar;
+	unsigned int ar_size, i;
+
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	size = max - min;
-	ar = malloc((size + 1) * sizeof(int));
-	i = 0;
-	n = min;
-	while (n <= max)
+	ar_size = nmemb * size;
+	ar = malloc(ar_size);
+	if (ar == NULL)
+		return (NULL);
+	while (i < ar_size)
 	{
-		ar[i] = n;
+		ar[i] = 0;
 		i++;
-		n++;
 	}
 	return (ar);
 }
